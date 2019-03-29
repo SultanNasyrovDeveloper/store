8e0dd3a63c5c5098e71e4818aefbd53c47fa6e61
Sultan Nasyrov <tightbtctrader@gmail.com>
2019-03-29 16:01:30 +0300

    Add Wish List model

--- store/wish_list/admin.py
+++ store/wish_list/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import WishList
+
+
+@admin.register(WishList)
+class WishListAdmin(admin.ModelAdmin):
+    pass
--- store/wish_list/models.py
+++ store/wish_list/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class WishList(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+    items = models.ManyToManyField('catalog.Product', blank=True, verbose_name='Products')
+
+    class Meta:
+        verbose_name = 'wish list'
+        verbose_name_plural = 'wish lists'
+
+    def __str__(self):
+        return 'Wish List {}, user {}'.format(self.id, self.user.username)
