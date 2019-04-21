494338988aa30d3722705813534ad4b806148984
Sultan Nasyrov <tightbtctrader@gmail.com>
2019-04-21 19:36:46 +0300

    Basic wishlist implementation

--- store/wish_list/models.py
+++ store/wish_list/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from store.catalog.models import Product
+
 
 class WishList(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
-    items = models.ManyToManyField('catalog.Product', blank=True, verbose_name='Products')
+    products = models.ManyToManyField('catalog.Product', blank=True, verbose_name='Products')
 
     class Meta:
         verbose_name = 'wish list'
@@ -12,3 +14,20 @@ class WishList(models.Model):
 
     def __str__(self):
         return 'Wish List {}, user {}'.format(self.id, self.user.username)
+
+    def add_product(self, product_id):
+        """ Add product to wish list by id """
+        product = Product.objects.get(id=product_id)
+        self.products.add(product)
+        self.save()
+
+    def delete_product(self, product_id):
+        """ Delete product from wish list by id """
+        product = Product.objects.get(id=product_id)
+        self.products.remove(product)
+        self.save()
+
+    def product_to_cart(self):
+        # TODO Implement when cart is ready
+        pass
+
