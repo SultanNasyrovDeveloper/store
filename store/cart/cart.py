8f6c0500fed0b7b5e56d8785c3364d32835140e2
Sultan Nasyrov <tightbtctrader@gmail.com>
2019-04-21 21:13:14 +0300

    Basic cart setup

---
+++ store/cart/cart.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.models import User
+
+
+
+
+
+class CartBase(object):
+
+    @property
+    def total(self):
+        return NotImplementedError
+
+    @property
+    def items_number(self):
+        return NotImplementedError
+
+    @property
+    def items_list(self):
+        return NotImplementedError
+
+    def add_item(self):
+        return NotImplementedError
+
+    def remove_item(self):
+        return NotImplementedError
+
+    def change_quantity(self):
+        return NotImplementedError
+
+
+class SessionCart(CartBase):
+    pass
+
+
+class DatabaseCart(CartBase):
+    pass
+
+
+def simple_cart_factory(user):
+    # TODO implement
+    # if user authenticated
+    # return DatabaseCart
+    # else
+    # return session cart
+    pass
